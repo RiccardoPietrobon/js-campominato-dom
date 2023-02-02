ac53e5654e77bb2951f3a3fa39557251f42a07c9
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-02 15:55:15 +0100

    16 numeri random ok

---
+++ JS/main.js
@@ -0,0 +1,112 @@
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+//Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+//Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti(ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Errata corrige: rendere le caselle "non cliccabili" e far finire la partita è un superbonus, non è richiesto dalla consegna!
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
+
+//Consigli del giorno: : party_wizard:
+//Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+
+//Ad esempio
+//Di cosa ho bisogno per generare i numeri ?
+//Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+//Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+//SUPERBONUS 1
+//Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+
+//SUPERBONUS 2
+//Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
+
+/************     CODICE     ********* */
+
+const my_button = document.getElementById("push");
+
+my_button.addEventListener(
+    "click",
+    function () {
+
+        const my_grid = document.getElementById("box");
+
+        const chose = document.getElementById("tipo_di_griglia").value;
+
+        generazione_griglia(my_grid, chose);
+
+    }
+)
+
+/*********** FUNZIONI **********/
+/* 
+* funzione per generare la mia griglia
+*
+*
+*
+ */
+function generazione_griglia(grid, dimensione_grid) {
+
+    //griglia vuota
+    grid.innerHTML = "";
+
+    //ciclo per generare il numero di celle desiderato
+    for (let i = 0; i < dimensione_grid; i++) {
+
+        //numeri da inserire
+        const square_text = [i + 1];
+        //console.log(square_text);
+
+        //aggiungo un div
+        const my_square = document.createElement("div");
+
+        //aggiungo le classi desiderate
+        my_square.classList.add("square");
+
+        if (dimensione_grid == "100") {
+
+            my_square.classList.add("square_10");
+
+        } else if (dimensione_grid == "81") {
+
+            my_square.classList.add("square_9");
+
+        } else {
+
+            my_square.classList.add("square_7");
+
+        }
+
+        //aggiungo il numero
+        my_square.append(square_text);
+
+        //funzione click
+        my_square.addEventListener(
+            "click",
+            function () {
+                this.classList.toggle("square_active");
+                console.log(this.innerHTML);
+            }
+        )
+
+        grid.append(my_square);
+    }
+
+}
+
+/* 
+* funzione per 16 numeri casuali
+*
+* */
+
+function bombs(dimensione_grid) {
+    const boxbombs = [];
+
+    while (boxbombs.lenght < 16) {
+        random_number = Math.floor(Math.random() * dimensione_grid) + 1;
+
+        if (!boxbombs.includes(random_number)) {
+            boxbombs.push(random_number);
+        }
+    }
+
+}
